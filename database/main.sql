a7db051c82f9d7763d0b3dbd72998b56cd091dae
Flamingraptor_70 <strubo.gt@gmail.com>
2023-12-08 22:45:08 +0200

    added json mock data, fixed db

--- database/main.sql
+++ database/main.sql
@@ -1,12 +1,7 @@
--- Create database
-
 DROP DATABASE IF EXISTS ice_dude;
 CREATE DATABASE ice_dude;
 USE ice_dude;
 
--- Creating new tables
-
--- Table for Users
 CREATE TABLE Users (
     UserID INT PRIMARY KEY AUTO_INCREMENT,
     UserLevel INT CHECK (UserLevel IN (1,2)) NOT NULL,
@@ -17,57 +12,28 @@ CREATE TABLE Users (
     created_at TIMESTAMP NOT NULL
 );
 
--- Table for Ice Cream item 
 CREATE TABLE IceCream (
-    IceCreamID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    IceCreamCategoryID INT NOT NULL,
+    IceCreamID INT PRIMARY KEY AUTO_INCREMENT,
     IceCreamName VARCHAR(255) NOT NULL,
     IceCreamDescription VARCHAR(255) NOT NULL,
     IceCreamPrice DECIMAL(8, 2) NOT NULL,
-    PRIMARY KEY (IceCreamID),
-    FOREIGN KEY (IceCreamCategoryID) REFERENCES IceCreamCategory(IceCreamCategoryID);
-    -- TODO: add image file
+    IceCreamImage VARCHAR(255), -- URL path to image
 );
 
-
--- Table for Ice Cream Category
-CREATE TABLE IceCreamCategory (
-    IceCreamCategoryID INT PRIMARY KEY AUTO_INCREMENT,
-    IceCreamCategoryName VARCHAR(255) NOT NULL,
-    IceCreamCategoryDescription VARCHAR(255) NOT NULL
-);
-
-
--- Table for Orders
 CREATE TABLE Orders (
     OrderID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     UserID INT,
     OrderDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (OrderID),
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
--- Table for Cart
 CREATE TABLE Cart (
     CartID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     OrderID INT NOT NULL,
     IceCreamID INT NOT NULL,
-    PRIMARY KEY (CartID),
     FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
 
--- Ice Cream Comments
-CREATE TABLE IceCream_Comments (
-    CommentID INT PRIMARY KEY AUTO_INCREMENT,
-    UserID INT,
-    IceCreamID INT,
-    Comment VARCHAR(255) NOT NULL,
-    CommentDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (UserID) REFERENCES Users(UserID),
-    FOREIGN KEY (IceCreamID) REFERENCES IceCream(IceCreamID)
-);
-
--- Quantity of different ice cream types
 CREATE TABLE IceCream_Quantity (
     IceCream_Quantity_ID INT PRIMARY KEY AUTO_INCREMENT,
     IceCreamID INT NOT NULL,
@@ -77,57 +43,3 @@ CREATE TABLE IceCream_Quantity (
     FOREIGN KEY (IceCreamID) REFERENCES IceCream(IceCreamID),
     FOREIGN KEY (CartID) REFERENCES Cart(CartID)
 );
-
--- Adding mock database data
-
--- Mock data for IceCreamCategory
-INSERT INTO IceCreamCategory (IceCreamCategoryName, IceCreamCategoryDescription)
-VALUES 
-('Ice Cream Cone', 'Mmmmm.. Coney Ice Cream'), 
-('Ice Cream Stick', 'Mmmmm.. Ice Cream Stick'), 
-('Ice Cream Cup', 'Mmmmm.. Ice Cream Cup'), 
-('Ice Cream Sandwich', 'Mmmmm.. Ice Cream Sandwich'), 
-('Ice Cream Bar', 'Mmmmm.. Ice Cream Bar');
-
--- Mock data for Users
--- Note: You need to replace 'username' and 'password' with the actual columns in your Users table
-INSERT INTO Users (username, password)
-VALUES 
-('user1', 'password1'), 
-('user2', 'password2');
-
--- Mock data for Orders
-INSERT INTO Orders (UserID)
-SELECT UserID FROM Users;
-
--- Mock data for IceCream
--- Note: You need to replace 'IceCreamName', 'IceCreamDescription', and 'IceCreamCategoryID' with the actual columns in your IceCream table
-INSERT INTO IceCream (IceCreamName, IceCreamDescription, IceCreamPrice, IceCreamCategoryID)
-VALUES 
-('Blueberry Ice Cream', 'Ice Cream with blueberry taste', 2.80, 1), 
-('Carrot Ice Cream', 'Mmmm... Carrot Ice', 2.90,  2);
-
--- Mock data for Cart
-INSERT INTO Cart (OrderID, IceCreamID, Quantity)
-SELECT OrderID, IceCreamID, 1 FROM Orders, IceCream;
-
--- Mock data for IceCream_Comments
--- Note: You need to replace 'CommentText' with the actual column in your IceCream_Comments table
-INSERT INTO IceCream_Comments (UserID, IceCreamID, CommentText)
-SELECT UserID, IceCreamID, 'This is a comment' FROM Users, IceCream;
-
-
-
--- Querying data
-
--- Query data
--- Get all ice creams in a specific category
-SELECT * FROM IceCream WHERE IceCreamCategoryID = 1;
-
--- Update data
--- Update the price of a specific ice cream
-UPDATE IceCream SET IceCreamPrice = 6.99 WHERE IceCreamID = 1;
-
--- Delete data
--- Delete a specific ice cream
-DELETE FROM IceCream WHERE IceCreamID = 1;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import {fileURLToPath} from 'url';
 
+
 const hostname = '127.0.0.1';
 const app = express(); // create express app
 const port = 3000;
