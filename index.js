6a5e4e2f6474939e9e546c1b0bd90a607ed3bee7
Flamingraptor_70 <strubo.gt@gmail.com>
2023-12-10 12:49:55 +0200

    added pages to server, deleted to need files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import path from 'path';
 import {fileURLToPath} from 'url';
+import path from 'path';
 
 
 const hostname = '127.0.0.1';
@@ -9,12 +9,39 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '/public')));
+// add middlewares
+app.use(express.static(path.join(__dirname, 'public', 'files')));
 
+// start with main page
 app.get('/', (req, res) => {
-  console.log('Here');
+  console.log('Main page');
   res.render('/public/index');
 });
+
+// menu page
+app.get('/menu', (req, res) => {
+  console.log('Catalog page');
+  res.sendFile(__dirname + '/public/pages/catalog/catalog.html');
+});
+
+// login page
+app.get('/login/', (req, res) => {
+  console.log('Login page');
+  res.sendFile(__dirname + '/public/pages/login/login.html');
+});
+
+// profile page
+app.get('/profile', (req, res) => {
+  console.log('Profile page');
+  res.sendFile(__dirname + '/public/pages/profile/profile.html');
+});
+
+// admins page
+app.get('/admin', (req, res) => {
+  console.log('Admins page');
+  res.sendFile(__dirname + '/admin/admin.html');
+});
+
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
