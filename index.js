3b62fa97a0aaea1cfedd9543ea3e4dfab54e4883
Khai Chan <strubo.gt@gmail.com>
2023-12-11 11:32:53 +0200

    useless commit

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-import express from 'express';
-import {fileURLToPath} from 'url';
-import path from 'path';
+/* eslint-disable linebreak-style */
+/* eslint-disable quotes */
+import express from "express";
+import {fileURLToPath} from "url";
+import path from "path";
 
-
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const app = express(); // create express app
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/public');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/public");
 
 // add middlewares
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // start with main page
-app.get('/', (req, res) => {
-  console.log('Main page');
-  res.render('index');
+app.get("/", (req, res) => {
+  console.log("Main page");
+  res.render("index");
 });
 
 // menu page
-app.get('/catalog', (req, res) => {
-  console.log('Catalog page');
-  res.render('pages/catalog/catalog');
+app.get("/catalog", (req, res) => {
+  console.log("Catalog page");
+  res.render("pages/catalog/catalog");
 });
 
 // login page
-app.get('/login/', (req, res) => {
-  console.log('Login page');
-  res.render('pages/login/login');
+app.get("/login/", (req, res) => {
+  console.log("Login page");
+  res.render("pages/login/login");
 });
 
 // profile page
-app.get('/profile', (req, res) => {
-  console.log('Profile page');
-  res.render('pages/profile/profile');
+app.get("/profile", (req, res) => {
+  console.log("Profile page");
+  res.render("pages/profile/profile");
 });
 
 // orders page
-app.get('/orders', (req, res) => {
-  console.log('Orders page');
-  res.render('pages/orders/orders');
+app.get("/orders", (req, res) => {
+  console.log("Orders page");
+  res.render("pages/orders/orders");
 });
 
 // admins page
-app.get('/admin', (req, res) => {
-  console.log('Admins page');
-  res.render('admin/admin');
+app.get("/admin", (req, res) => {
+  console.log("Admins page");
+  res.render("admin/admin");
 });
-
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
